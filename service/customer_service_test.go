ab0e19dfaaf136d0adf46e643297f6e29f7294a2
karl-huangjm <1259040327@qq.com>
2018-07-01 00:18:19 +0800

    update go test & travis CI

--- service/customer_service_test.go
+++ service/customer_service_test.go
@@ -1,63 +1,73 @@
 package service
 
 import (
-	"fmt"
+	"reflect"
 	"testing"
+
+	"github.com/karl-jm-huang/golang-CI/entity"
 )
 
-//测试插入用户
-func Test_CustomerRegister(t *testing.T) {
-
-	shouldSuccess := []struct {
+func TestCustomerRegister(t *testing.T) {
+	type args struct {
 		name          string
 		password      string
 		restaurant_id int
 		phone         string
-		expected      bool
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    bool
+		wantErr bool
 	}{
-		{"hzw12", "123", 1, "13719179588", true},
-		{"hzw23", "123", 1, "", false},
+	// TODO: Add test cases.
 	}
-	for _, ts := range shouldSuccess {
-		if real, _ := CustomerRegister(ts.name, ts.password, ts.restaurant_id, ts.phone); real != ts.expected {
-			t.Errorf("fail in " + ts.name + ts.phone)
-		} else {
-			t.Log("success in " + ts.name + ts.phone)
-		}
-	}
-
-}
-
-//在插入的基础上测试查询所有用户
-func Test_ListAllCustomers(t *testing.T) {
-	real := ListAllCustomers()
-	for _, item := range real {
-		fmt.Println(item.Name)
-	}
-	if len(real) == 2 {
-		t.Log("success in ")
-	} else {
-		t.Errorf("fail in ")
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := CustomerRegister(tt.args.name, tt.args.password, tt.args.restaurant_id, tt.args.phone)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("CustomerRegister() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("CustomerRegister() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
 
-//在插入的基础上测试通过Name查询用户
-func Test_GetCustomerByName(t *testing.T) {
-	shouldSuccess := []struct {
-		cname    string
-		expected bool
+func TestListAllCustomers(t *testing.T) {
+	tests := []struct {
+		name string
+		want []entity.Customer
 	}{
-		{"hzw12", true},
-		{"hzw2", false},
+	// TODO: Add test cases.
 	}
-	for _, ts := range shouldSuccess {
-		if real := GetCustomerByName(ts.cname); real != nil && ts.expected == true ||
-			real == nil && ts.expected == false {
-			t.Logf("success in " + ts.cname)
-		} else {
-			t.Errorf("fail in " + ts.cname)
-		}
-
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ListAllCustomers(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ListAllCustomers() = %v, want %v", got, tt.want)
+			}
+		})
 	}
+}
 
+func TestGetCustomerByName(t *testing.T) {
+	type args struct {
+		cname string
+	}
+	tests := []struct {
+		name string
+		args args
+		want *entity.Customer
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := GetCustomerByName(tt.args.cname); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetCustomerByName() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
